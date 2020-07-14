93b04f95b401eb72d465e15ad1d63ce976a14604
Hector Castillo Martinez <39133894+hcastillomartinez@users.noreply.github.com>
2020-07-13 19:20:13 -0500

    Add command line functionality

--- transcript_scraper.py
+++ transcript_scraper.py
@@ -2,33 +2,58 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import sys
 
-
-# replace with your own web driver executable
+# place your own path to webdriver
 path = r'C:\\Users\\damia\\Downloads\\geckodriver\\geckodriver.exe'
-driver = webdriver.Firefox(executable_path=path)
-driver.get('https://www.youtube.com/watch?v=4nzI4RKwb5I')
-moreActionsButton = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath('//button[@id="button"][@aria-label="More actions"]'))
 
-# select more actions button
-moreActionsButton.click()
+def get_title(url):
+    driver = webdriver.Firefox(executable_path=path)
+    driver.get(url)
+    title = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath('//yt-formatted-string[@class="style-scope ytd-video-primary-info-renderer"]'))
+    ret = title.text
+    driver.close()
+    driver.quit()
+    return ret
 
-openTrans = WebDriverWait(driver,10).until(lambda driver: driver.find_element_by_xpath('//paper-item[@class="style-scope ytd-menu-service-item-renderer"][@role="option"]'))
 
-# open transcript window
-openTrans.click()
+def get_transcript(url):
+    driver = webdriver.Firefox(executable_path=path)
+    driver.get(url)
+    moreActionsButton = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath('//button[@id="button"][@aria-label="More actions"]'))
 
-# make sure transcript window is open
-testTrans = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath('//div[@class="cue style-scope ytd-transcript-body-renderer"][@role="button"]'))
+    # select more actions button
+    moreActionsButton.click()
 
-transcript = driver.find_elements_by_xpath('//div[@class="cue style-scope ytd-transcript-body-renderer"][@role="button"]')
+    openTrans = WebDriverWait(driver,10).until(lambda driver: driver.find_element_by_xpath('//paper-item[@class="style-scope ytd-menu-service-item-renderer"][@role="option"]'))
 
-f = open("transcript.txt","w+")
+    # open transcript window
+    openTrans.click()
 
-for element in transcript:
-    f.write(element.text)
-    f.write('\n')
+    # make sure transcript window is open
+    testTrans = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath('//div[@class="cue style-scope ytd-transcript-body-renderer"][@role="button"]'))
 
-f.close()
-driver.close()
-driver.quit()
+    transcript = driver.find_elements_by_xpath('//div[@class="cue style-scope ytd-transcript-body-renderer"][@role="button"]')
+    title = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath('//yt-formatted-string[@class="style-scope ytd-video-primary-info-renderer"]')).text
+    
+    # write out transcript to file named after the video title
+    f = open(title+".txt", "w+")
+    for element in transcript:
+        f.write(element.text)
+        f.write('\n')
+    
+    f.close()
+
+    # close the driver
+    driver.close()
+    driver.quit()
+
+    return title
+
+
+if len(sys.argv) < 2:
+    print('Not enough arguments.')
+else:
+    url = sys.argv[1]
+    # prints title
+    print(get_title(url))
